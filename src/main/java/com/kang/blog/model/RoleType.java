b32dc3a94b8bb1cdbb8cbfd62670446626a8db74
joonhyuck <jh83370@naver.com>
2022-10-25 04:34:53 +0900

    feat: User Entity

---
+++ src/main/java/com/kang/blog/model/RoleType.java
@@ -0,0 +1,5 @@
+package com.kang.blog.model;
+
+public enum RoleType {
+    USER, ADMIN
+}
---
+++ src/main/java/com/kang/blog/model/User.java
@@ -0,0 +1,38 @@
+package com.kang.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //Jpa의 기본 전략이 아닌 연결된 DB의 넘버링 전략 따름.(mysql이면 auto_increment, oracle이면 sequence사용)
+    private int id;
+
+    @Column(nullable = false, length = 30)  //30자 제한
+    private String username;
+
+    @Column(nullable = false, length = 100)  //비번 해쉬화하기 위해 넉넉히 100자.
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    @CreationTimestamp
+    private Timestamp createDate;            //repository.save() 후-> 데이터 createDate에 저장 후 -> db에 insert.
+}
