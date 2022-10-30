1cc2bf0f895e95e2d8c6f0c8268cfe08c7237b49
joonhyuck <jh83370@naver.com>
2022-10-30 22:47:28 +0900

    refactor: 예외 처리 페이지(add responseDto)

---
+++ src/main/java/com/kang/blog/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.kang.blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResponseDto<T> {
+    int status;
+    T data;
+}
--- src/main/java/com/kang/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kang/blog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.kang.blog.handler;
 
+import com.kang.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,8 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(value = Exception.class)          //모든 예외 탐지
-    public String handleException(Exception e){
-        return "<h1>"+e.getMessage()+"</h1>";          //예외 메세지 출력
+    public ResponseDto<String> handleException(Exception e){
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        //return "<h1>"+e.getMessage()+"</h1>";          //예외 메세지 출력
     }
 }
