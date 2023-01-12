2b2206b98231e9445d5fabe5ac770fb8bbe0ccb9
joonhyuck <jh83370@naver.com>
2023-01-12 16:49:39 +0900

    refactor: ReponseDto

--- src/main/java/com/kang/blog/dto/ResponseDto.java
+++ src/main/java/com/kang/blog/dto/ResponseDto.java
@@ -7,9 +7,21 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class ResponseDto<T> {
-    int status;
+    int statusCode;
     T data;
+
+    //상태 코드만 리턴하고 싶을 때 사용.
+    public ResponseDto(int statusCode){
+        super();
+        this.statusCode = statusCode;
+    }
+
+    //상태 코드, 데이터를 리턴 하고 싶을 때 사용.
+    public ResponseDto(int statusCode, T data){
+        super();
+        this.statusCode =statusCode;
+        this.data =data;
+    }
 }
