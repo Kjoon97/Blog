e95d2b4a5990bc912045482ea338faabbfe9f1cc
joonhyuck <jh83370@naver.com>
2023-01-21 01:54:46 +0900

    ci cd

--- src/main/java/com/kang/blog/BlogApplication.java
+++ src/main/java/com/kang/blog/BlogApplication.java
@@ -3,11 +3,13 @@ package com.kang.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing  // 생성/수정 날짜,시간 자동화
 @SpringBootApplication
+@PropertySource("classpath:/env.properties")
 public class BlogApplication {
 
 	//비밀번호 해쉬 인코딩 매소드 빈 등록
